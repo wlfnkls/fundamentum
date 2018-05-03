55c7790bcde36ae0632243f403da59ef6fb95938
niklas <niklas.schellhoeh@gmail.com>
2018-05-03 22:54:15 +0200

    Class declaration, ES6 arrow function <3

--- src/test.js
+++ src/test.js
@@ -5,12 +5,12 @@
 
 import $ from 'jquery';
 
-export default function test() {
+export default () => {
   let $container = $('.container');
-  let $headline = $('<h2 class=headline></h2>');
+  let $headline = $('<h2 class=test-headline></h2>');
 
   $(function () {
     $headline.text('Welcome to Fundamentum!');
     $container.append($headline);
   });
-}
+};
