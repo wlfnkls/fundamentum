1412842b29ca447774dfcdb2a8b44f5c5fcf9186
niklas <niklas.schellhoeh@gmail.com>
2018-05-06 14:12:28 +0200

    Added module to compile css and export as bundled file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s*)scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({ filename: 'css/styles.css' }),
+  ]
 };
